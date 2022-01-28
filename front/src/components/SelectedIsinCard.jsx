a9a3ed57908d87e0e169f18501eabe3c0095d595
kfivap KIRILL <kfivap@gmail.com>
2022-01-29 00:47:13 +0300

    inwork

--- front/src/components/SelectedIsinCard.jsx
+++ front/src/components/SelectedIsinCard.jsx
@@ -7,26 +7,36 @@ const SelectedIsinCard = () => {
     console.log(currentIsinDerivativeData)
     if (!currentIsin) return null
     return (
-        <div>
+        <div className='selected-isin-card'>
             <h3>{currentIsin?.isin}</h3>
             <h5>{currentIsin?.name}</h5>
-            Data:
             date: {currentIsinDerivativeData?.fiz?.date}
             <br />
-            <p>
-                fiz <br />
-                clients in long: {currentIsinDerivativeData?.fiz?.clients_in_long} <br />
-                long position: {currentIsinDerivativeData?.fiz?.long_position} <br />
-                clients in short: {currentIsinDerivativeData?.fiz?.clients_in_short} <br />
-                clients short position: {currentIsinDerivativeData?.fiz?.short_position} <br />
-            </p>
-            <p>
-                legal <br />
-                clients in long: {currentIsinDerivativeData?.legal?.clients_in_long}<br />
-                clients in short: {currentIsinDerivativeData?.legal?.clients_in_short}<br />
-                clients long position {currentIsinDerivativeData?.legal?.long_position}<br />
-                clients short position {currentIsinDerivativeData?.legal?.short_position}<br />
-            </p>
+            <div className='isin-info-container'>
+
+                <div className='isin-info-elem'>
+                    <b>fiz</b> <br />
+                    clients in long: {currentIsinDerivativeData?.fiz?.clients_in_long} <br />
+                    clients in short: {currentIsinDerivativeData?.fiz?.clients_in_short} <br />
+                    long positions: {currentIsinDerivativeData?.fiz?.long_position} <br />
+                    short positions: {currentIsinDerivativeData?.fiz?.short_position} <br />
+                </div>
+                <div className='isin-info-elem'>
+                    <b>legal</b> <br />
+                    clients in long: {currentIsinDerivativeData?.legal?.clients_in_long}<br />
+                    clients in short: {currentIsinDerivativeData?.legal?.clients_in_short}<br />
+                    long positions: {currentIsinDerivativeData?.legal?.long_position}<br />
+                    short positions: {currentIsinDerivativeData?.legal?.short_position}<br />
+                </div>
+                <div className='isin-info-elem'>
+                    <b>difference</b> <br />
+                    legal/fiz clients in long {(currentIsinDerivativeData?.legal?.clients_in_long / currentIsinDerivativeData?.fiz?.clients_in_long).toFixed(2)}<br />
+                    legal/fiz clients in short: {(currentIsinDerivativeData?.legal?.clients_in_short / currentIsinDerivativeData?.fiz?.clients_in_long).toFixed(2)}<br />
+                    legal/fiz long position {(currentIsinDerivativeData?.legal?.long_position / currentIsinDerivativeData?.fiz?.long_position).toFixed(2)}<br />
+                    legal/fiz short position {(currentIsinDerivativeData?.legal?.short_position / currentIsinDerivativeData?.fiz?.short_position).toFixed(2)}<br />
+                </div>
+            </div>
+
 
         </div>
     );
